59a9aa07503252a541f92552c11a71bd0f43d696
sachinn29 <112768099+sachinn29@users.noreply.github.com>
2024-02-03 01:03:12 +0530

    Add files via upload

---
+++ taskm1_t3.ino
@@ -0,0 +1,46 @@
+#define pirPin 2
+#define buttonPin 3  
+#define motionLedPin 8  
+#define buttonLedPin 4  
+
+volatile bool isMotionDetected = false;
+volatile bool isButtonPressed = false;
+
+void interruptPIR() {
+    isMotionDetected = true;
+}
+
+void interruptButton() {
+    isButtonPressed = true;
+}
+
+void setup() {
+    pinMode(pirPin, INPUT);
+    pinMode(buttonPin, INPUT_PULLUP);  
+    pinMode(motionLedPin, OUTPUT);
+    pinMode(buttonLedPin, OUTPUT);
+    Serial.begin(9600);
+
+    attachInterrupt(digitalPinToInterrupt(pirPin), interruptPIR, RISING);
+    attachInterrupt(digitalPinToInterrupt(buttonPin), interruptButton, FALLING);
+}
+
+void loop() {
+    if (isMotionDetected) {
+        digitalWrite(motionLedPin, HIGH);
+        Serial.println("Motion detected! Turning on Motion LED.");
+        isMotionDetected = false;
+    } else {
+        digitalWrite(motionLedPin, LOW);
+    }
+
+    if (isButtonPressed) {
+        digitalWrite(buttonLedPin, HIGH);
+        Serial.println("Button pressed! Turning on Button LED.");
+        isButtonPressed = false;
+    } else {
+        digitalWrite(buttonLedPin, LOW);
+    }
+
+    delay(1000);
+}
